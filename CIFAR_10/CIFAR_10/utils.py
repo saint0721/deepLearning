d4511d47bcaa0e62bb03aee042e735b70fa97154
saint0721 <sikim0721@gmail.com>
2024-09-15 19:58:12 +0900

    안쓰는 코드 삭제

--- CIFAR_10/CIFAR_10/utils.py
+++ CIFAR_10/CIFAR_10/utils.py
@@ -13,29 +13,6 @@ def unpickle(file):
     return data_dict
 
 
-class ModelTrainer:
-    def __init__(self, model, classes, device="mps"):
-        self.model = model
-        self.classes = classes
-        self.device = device
-        self.model.to(self.device)
-        self.criterion = nn.CrossEntropyLoss()
-
-    def imshow(self, images, labels, predictions):
-        fig, axes = plt.subplots(2, 5, figsize=(12, 6))
-        axes = axes.flatten()
-
-        for img, label, pred, ax in zip(images, labels, predictions, axes):
-            img = img / 2 + 0.5  # 정규화된 이미지를 원래 범위로 변환
-            npimg = np.clip(img.numpy().transpose((1, 2, 0)), 0, 1)
-            ax.imshow(npimg)
-            ax.set_title(
-                f"True: {self.classes[label]} \n Pred: {self.classes[pred]}",
-                fontsize=20,
-            )
-            ax.axis("off")
-        plt.tight_layout()
-        plt.close()
 
 
 def load_train_cifar_data(cifar_dir, batch_range):
